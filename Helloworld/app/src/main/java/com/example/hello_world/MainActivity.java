21993a6235b95aa1435eb2ea286e032ee75c62bf
ehojune <ehojune@gmail.com>
2021-07-02 16:18:01 +0900

    tab2 almost done

--- Helloworld/app/src/main/java/com/example/hello_world/MainActivity.java
+++ Helloworld/app/src/main/java/com/example/hello_world/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.hello_world;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -9,17 +11,47 @@ import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
-import androidx.fragment.app.ListFragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+
 public class MainActivity extends AppCompatActivity {
-    static final String[] LIST_MENU = {"LIST1", "LIST2", "LIST3"} ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // ... 코드 계속
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, LIST_MENU);
-        ListFragment menuListFrgmt = (ListFragment) getSupportFragmentManager().findFragmentById(R.id.menulistfragment);
-        menuListFrgmt.setListAdapter(adapter);
+        ViewPager vp = findViewById(R.id.viewpager);
+        VPAdapter adapter = new VPAdapter(getSupportFragmentManager());
+        vp.setAdapter(adapter);
+
+        // 연동
+        TabLayout tab = findViewById(R.id.tab);
+        tab.setupWithViewPager(vp);
+
+        //Image 추가지만 의미가 읎다..
+        //ArrayList<Integer> images = new ArrayList<>();
+        //images.add(R.drawable.cal);
+        //images.add(R.drawable.sea);
+        //images.add(R.drawable.set); No image
+
+        //for(int i=0; i<3; i++) tab.getTabAt(i).setIcon(images.get(i));
+
+
+
 
     }
+
+    public void displayPicture(View v) {
+        int id = v.getId();
+        LinearLayout layout = (LinearLayout) v.findViewById(id);
+        String tag = (String) layout.getTag();
+
+
+        Intent it = new Intent(this, Picture.class);
+        it.putExtra("it_tag", tag);
+        startActivity(it);
+    }
+
+
 }
