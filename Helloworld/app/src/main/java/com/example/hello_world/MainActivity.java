659c15628b6558839f3f5e320bbee117a3928877
JAK-CAL <big01ad@kaist.ac.kr>
2021-07-07 01:10:13 +0900

    Real Final?

--- Helloworld/app/src/main/java/com/example/hello_world/MainActivity.java
+++ Helloworld/app/src/main/java/com/example/hello_world/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.hello_world;
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.preference.PreferenceManager;
 import androidx.viewpager.widget.ViewPager;
 
@@ -18,6 +23,28 @@ import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
     static boolean next = true;
+
+    final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 권한 획득 성공
+                } else {
+                    // 권한 획득 실패
+                }
+                return;
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +55,8 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = sharedPreferences.edit();
         vp.setAdapter(adapter);
 
-        // 연동
+
+
         TabLayout tab = findViewById(R.id.tab);
         tab.setupWithViewPager(vp);
 
@@ -46,6 +74,35 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     alertDialog.setMessage("카이스트 신입생을 위해 만든 앱으로, 시간표 등의 정보를 제공합니다!");
+                    alertDialog.setMessage("이 앱은 전화를 위한 권한이 필요합니다!");
+
+                    if (ContextCompat.checkSelfPermission(MainActivity.this,
+                            Manifest.permission.READ_CONTACTS)
+                            != PackageManager.PERMISSION_GRANTED) {
+
+                        // Should we show an explanation?
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                                Manifest.permission.READ_CONTACTS)) {
+
+                            // Show an expanation to the user *asynchronously* -- don't block
+                            // this thread waiting for the user's response! After the user
+                            // sees the explanation, try again to request the permission.
+
+                        } else {
+
+                            // No explanation needed, we can request the permission.
+
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[]{Manifest.permission.READ_CONTACTS},
+                                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                            // app-defined int constant. The callback method gets the
+                            // result of the request.
+                        }
+                    }
+
+
                     if(next) {
                         next = false;
                         alertDialog.setPositiveButton("시작!", new DialogInterface.OnClickListener() {
@@ -64,6 +121,10 @@ public class MainActivity extends AppCompatActivity {
             editor.putBoolean("IS_FIRST_RUN", false);
             editor.apply();
         }
+
+
+
+
 
     }
 
